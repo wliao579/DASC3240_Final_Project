377f12099fdfc177037113077d6dbcbe06f97252
nick <nick+noreply@example.com>
2026-05-01 00:57:38 +0800

    Added visual

--- app.R
+++ app.R
@@ -1,51 +1,72 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
+library(ggplot2)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+method_dict = list(
+  "Two pointers" = "avg_two_pointers", 
+  "Three pointers" = "avg_three_pointers",
+  "Free throws" = "avg_free_throws",
+  "Field Goal" = "avg_field_goals"
+  )
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+basket <- read.csv("basketball_processed.csv")
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+method_dict = list(
+  "Two pointers" = "avg_two_pointers", 
+  "Three pointers" = "avg_three_pointers",
+  "Free throws" = "avg_free_throws",
+  "Field Goal" = "avg_field_goals"
 )
 
-# Define server logic required to draw a histogram
+ui <- fluidPage(
+  # Application title
+  titlePanel("Players by Scoring Methods per Game"), # Do not forget to add ","
+  # Use a sidebar layout
+  sidebarLayout(
+    # Sidebar component
+    sidebarPanel(
+      selectInput(
+        inputId = "method",
+        label = "Method",
+        choices = names(method_dict)
+      ),
+      sliderInput(
+        inputId = "slice",
+        label = "Rank range",
+        width = "600px",
+        min = 1,
+        max = length(basket$player_name),
+        value = c(1, 5)),
+      width = 6),
+    # Main panel component
+    mainPanel(
+      plotOutput("barPlot", height = "800px"), width = 6
+    )
+  )
+)
+
+# Server
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  output$barPlot <- renderPlot({
+    basket %>%
+      arrange(desc(!!sym(method_dict[[input$method]]))) %>%
+      slice(input$slice[1]:input$slice[2]) %>%
+      ggplot() +
+      geom_col(
+        aes(
+          !!sym(method_dict[[input$method]]),
+          player_name,
+          y = reorder(player_name, !!sym(method_dict[[input$method]]))
+        ), fill = "deepskyblue4") +
+      xlab(paste("Average", tolower(input$method), "per game")) +
+      ylab("Player") +
+      ggtitle(
+        paste("Top player by", tolower(input$method)),
+        subtitle = paste("Now showing player", input$slice[1], "to", input$slice[2])
+        )
+  })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run Shiny app
+shinyApp(ui, server)
+
